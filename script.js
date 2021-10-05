987f49e38575793a836c69aaeeba3be2fa4603db
Matt Eva <matteweva@gmail.com>
2021-10-05 09:29:44 -0700

    style changes

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ function reveal(){
     
 }
 
+
+
+
+
 window.addEventListener('resize', function() {
-    
+    document.getElementById('width').innerText = window.innerWidth;
+    document.getElementById('height').innerText = window.innerHeight;
+   if (window.innerWidth > 1042) {
+        document.getElementById('description').style.fontSize = '30px';
+    }
+   else if (932 < window.innerWidth < 1042) {
+        document.getElementById('description').style.fontSize = '25px';
+    }
+    else if (window.innerWidth < 932) {
+        document.getElementById('description').style.fontSize = '10px';
+    }
 })

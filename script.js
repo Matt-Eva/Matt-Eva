5d235b1403f6a331b305e9f1d4ca661f779d9659
Matt Eva <matteweva@gmail.com>
2021-10-05 11:20:51 -0700

    style changes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function reveal(){
 }
 
 
-function windowSize() {
+function introSize() {
     if (window.innerWidth > 1042) {
         document.getElementById('head').style.fontSize = '40px';
         let x = document.getElementsByClassName('description');
@@ -44,16 +44,81 @@ function windowSize() {
         let x = document.getElementsByClassName('description');
         for(const element of x) {
             element.style.fontSize = '18px';
+            
             }
     }
 }
 
-windowSize();
+introSize();
 
+function labelSize() {
+    if (window.innerWidth > 1206) {
+        let x = document.getElementsByClassName('label');
+        for(const element of x) {
+            element.style.margin = '10px 100px 10px 100px';
+        }
+        let y = document.getElementsByClassName('label');
+        for(const element of y) {
+            element.style.fontSize = '20px';
+        }
+    }
+    else if(window.innerWidth <= 1206 && window.innerWidth > 1078) {
+        let x = document.getElementsByClassName('label');
+        for(const element of x) {
+            element.style.margin = '10px 80px 10px 80px';
+        }
+        let y = document.getElementsByClassName('label');
+        for(const element of y) {
+            element.style.fontSize = '20px';
+        }
+    }
+    else if(window.innerWidth <= 1078 && window.innerWidth > 900) {
+        let x = document.getElementsByClassName('label');
+        for(const element of x) {
+            element.style.margin = '10px 50px 10px 50px';
+        }
+        let y = document.getElementsByClassName('label');
+        for(const element of y) {
+            element.style.fontSize = '20px';
+        }
+    }
+    else if(window.innerWidth <= 900 && window.innerWidth > 718) {
+        let x = document.getElementsByClassName('label');
+        for(const element of x) {
+            element.style.margin = '10px 20px 10px 20px';
+        }
+        let y = document.getElementsByClassName('label');
+        for(const element of y) {
+            element.style.fontSize = '20px';
+        }
+    }
+    else if(window.innerWidth <= 718 && window.innerWidth > 599) {
+        let x = document.getElementsByClassName('label');
+        for(const element of x) {
+            element.style.margin = '10px 0px 10px 0px';
+        }
+        let y = document.getElementsByClassName('label');
+        for(const element of y) {
+            element.style.fontSize = '20px';
+        }
+    }
+    else if(window.innerWidth <= 599) {
+        let x = document.getElementsByClassName('label');
+        for(const element of x) {
+            element.style.margin = '10px 0px 10px 0px';
+        }
+        let y = document.getElementsByClassName('label');
+        for(const element of y) {
+            element.style.fontSize = '16px';
+        }
+    }
+}
 
+labelSize();
 
 window.addEventListener('resize', function() {
     document.getElementById('width').innerText = window.innerWidth;
     document.getElementById('height').innerText = window.innerHeight;
-    windowSize();
+    introSize();
+    labelSize();
 })
